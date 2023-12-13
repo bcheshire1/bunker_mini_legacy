534a9d7d33a2fdcbdb03c5c8ac5badfd81da7cb1
Ben Cheshire <b.cheshire1@lancaster.ac.uk>
2023-12-13 17:42:08 +0000

    small edits

--- launch/launch_nav2.launch.py
+++ launch/launch_nav2.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -48,18 +49,17 @@ def generate_launch_description():
     slam_toolbox = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('slam_toolbox'),'launch','online_async_launch.py'
-                )]), launch_arguments={'params_file': './src/bunker_mini/config/mapper_params_online_async.yaml',
+                )]), launch_arguments={'slam_params_file': './src/bunker_mini/config/mapper_params_online_async.yaml',
                                        'use_sim_time': use_sim_time,
                                        }.items()
     )
     
-    #navigation2 = IncludeLaunchDescription(
-    #            PythonLaunchDescriptionSource([os.path.join(
-    #                get_package_share_directory('nav2_bringup'),'launch','navigation_launch.py'
-    #            )]), launch_arguments={'use_sim_time': use_sim_time,
-    #                                   'map_subscribe_transient_local': 'true',
-    #                                   }.items()
-    #)
+    navigation2 = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('nav2_bringup'),'launch','navigation_launch.py'
+                )]), launch_arguments={'params_file': './src/bunker_mini/config/nav2_params.yaml',
+                                       }.items()
+    )
 
 
     # Launch them all!
@@ -69,5 +69,5 @@ def generate_launch_description():
         spawn_entity,
         rviz2,
         slam_toolbox,
-        #navigation2,
+        navigation2,
     ])
