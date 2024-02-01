cd2553ec04407a630137d8eae87e030fe898d6b6
Nathan Leadbetter <149001851+n-leadbetter@users.noreply.github.com>
2024-02-01 16:00:51 +0000

    Update pyGIserver.py

--- PiGI-master/PiGI-master/software/pyGIserver.py
+++ PiGI-master/PiGI-master/software/pyGIserver.py
@@ -2,12 +2,12 @@
 import logging
 from pyGI.configurator import cfg
 
-#setup logging
+# Setup logging
 log = logging.getLogger()
-log.setLevel(cfg.get('logging','level'))
+log.setLevel(cfg.get('logging', 'level'))
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
-if cfg.getboolean('logging','write_file'):
-    filehandler = logging.FileHandler(cfg.get('logging','filename'))
+if cfg.getboolean('logging', 'write_file'):
+    filehandler = logging.FileHandler(cfg.get('logging', 'filename'))
     filehandler.setFormatter(formatter)
     log.addHandler(filehandler)
 streamhandler = logging.StreamHandler()
@@ -16,19 +16,20 @@ log.addHandler(streamhandler)
 
 if __name__ == "__main__":
     log.info("Starting pyGIserver")
-    from pyGI import geigercounter,geigerserver,geigerlog
+    from pyGI import geigercounter, geigerserver, geigerlog
     try:
-        #get last totalcount from db
-        (last_total,last_total_dtc) = geigerlog.get_last_totalcount()
-        log.info("Last total: %d, total_dtc: %d"%(last_total, last_total_dtc))
-        #start geigercounter
+        # Get last totalcount from db
+        (last_total, last_total_dtc) = geigerlog.get_last_totalcount()
+        log.info("Last total: %d, total_dtc: %d" % (last_total, last_total_dtc))
+        # Start geigercounter
         geiger = geigercounter.Geigercounter(total=last_total, total_dtc=last_total_dtc)
-        
-        #start geigercounter logging
+
+        # Start geigercounter logging
         geigerlog = geigerlog.GeigerLog(geiger)
-    
-        #start the bottle server stuff
-        geigerserver.start(geiger,geigerlog)
-        
+
+        # Start the bottle server stuff
+        geigerserver.start(geiger, geigerlog)
+
     except KeyboardInterrupt:
         log.info("Stopping pyGIserver")
+
