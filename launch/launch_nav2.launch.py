4df158862afa0b40d3c67b670265af22b2bebb0c
bcheshire1 <147816634+bcheshire1@users.noreply.github.com>
2024-02-02 12:48:34 +0000

    Add files via upload

--- launch/launch_nav2.launch.py
+++ launch/launch_nav2.launch.py
@@ -4,7 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
